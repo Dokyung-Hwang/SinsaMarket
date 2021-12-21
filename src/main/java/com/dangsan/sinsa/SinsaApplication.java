8881d27e3674c8297d5dc580ed83e02633ea66e3
unknown <hdokyung94@gmail.com>
2021-12-21 11:18:49 +0900

    create project

---
+++ src/main/java/com/dangsan/sinsa/SinsaApplication.java
@@ -0,0 +1,13 @@
+package com.dangsan.sinsa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SinsaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SinsaApplication.class, args);
+	}
+
+}
